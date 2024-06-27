510ec143dec59e96f64de161c67b6e37d88dcbb1
karthik v <karthikv@karthiks-mbp.in.ibm.com>
2024-06-27 14:00:55 +0530

    added multiple packages

--- new/new.go
+++ new/new.go
@@ -1,7 +1,13 @@
 package new
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/karthik-vijayan93/go-operator-kv/kv"
+)
 
 func Kvprint(s string) string {
+	fmt.Println(kv.Kv())
 	return strings.ToUpper(s)
 }
